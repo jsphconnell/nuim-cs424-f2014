acb8c6dfcb83258b9547f393872d84a4efc8d5ce
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-11-11 09:20:43 +0000

    apply peephole optimization

--- lect14.scm
+++ lect14.scm
@@ -6,16 +6,17 @@
 
 ;;; "Machine Code" for F
 
+;;; Because "ret" = pop address off stack and jump to it ...
+;;; apply "peephole optimization"
+;;;   {push A ; jmp X ; A: ; ret} ⤳ {jmp X}
+
 ;; f:	; x is in r1
 ;;	; marshal args to u (1st arg to u is 1st arg to us)
 ;;	push f1
 ;;	jmp u
 ;; f1:
 ;;	; marshal args to g (return value from u to 1st arg to g)
-;;	push f2
 ;;	jmp g
-;; f2:
-;;	ret
 
 ;; g:	; arg in r3
 ;;	...
