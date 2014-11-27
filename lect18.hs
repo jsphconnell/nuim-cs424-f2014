9caaa0ceb10926ae088a19f38f680f741288bfd9
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-11-27 14:27:26 +0000

    Mebe Double to Mebe a

--- lect18.hs
+++ lect18.hs
@@ -3,10 +3,10 @@
 --   Joost (aka Just)
 --   Nada (aka Nothing)
 
-data Mebe = Joost Double | Nada
+data Mebe a = Joost a | Nada
           deriving Show
 
-liftMebe2 :: (Double -> Double -> Double) -> Mebe -> Mebe -> Mebe
+liftMebe2 :: (a -> b -> c) -> Mebe a -> Mebe b -> Mebe c
 liftMebe2 f (Joost x) (Joost y) = Joost (f x y)
 liftMebe2 f Nada _ = Nada
 liftMebe2 f _ Nada = Nada
@@ -16,27 +16,9 @@ liftMebe2 f _ Nada = Nada
 -- addMebe x = liftMebe2 (+) x -- eta reduce prev line
 addMebe = liftMebe2 (+) -- eta reduce prev line
 
-{-
-*Main> Joost 2 `addMebe` Joost 3
-Joost 5.0
-*Main> Joost 2 `addMebe` Nada
-Nada
-*Main> Nada `addMebe` Joost 3
-Nada
-*Main> Nada `addMebe` Nada
-Nada
-
-*Main> liftMebe (*) (Joost 2) (Joost 3)
-Joost 6.0
--}
-
 -- version of "reciprocal" for Mebe:
 
-recipMebe Nada = Nada
-recipMebe (Joost 0) = Nada
-recipMebe (Joost x) = Joost (1 / x)
-
-liftMebe1 :: (Double -> Double) -> Mebe -> Mebe
+liftMebe1 :: (a -> b) -> Mebe a -> Mebe b
 liftMebe1 f Nada = Nada
 liftMebe1 f (Joost x) = Joost (f x)
 
@@ -46,7 +28,7 @@ liftMebe1 f (Joost x) = Joost (f x)
 -- because they disagree on
 --  recipMebe (Joost 0)
 
-chainToMebe :: (Double -> Mebe) -> Mebe -> Mebe
+chainToMebe :: (a -> Mebe b) -> Mebe a -> Mebe b
 chainToMebe f Nada = Nada
 chainToMebe f (Joost x) = f x
 
